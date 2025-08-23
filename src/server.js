9ace9445f742f06ec58aaa5b18411bccff80b5d0
ratatatcode <pogisijamesmagnaye@gmail.com>
2025-08-23 14:06:04 +0800

    Initial setup

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+    console.log(`Listening to ${PORT}...`);
+});
