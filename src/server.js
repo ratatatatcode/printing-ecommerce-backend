368364ad7f850ccc687e83ebd716ed1ed87ef4be
ratatatcode <pogisijamesmagnaye@gmail.com>
2025-08-25 18:19:02 +0800

    Add CORS

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
-app.use(express.json());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
 
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5001;
