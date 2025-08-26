e7e2bad6f72f8d49f5e25ddb2eaddda9f23ab5c7
ratatatcode <pogisijamesmagnaye@gmail.com>
2025-08-27 02:20:16 +0800

    Update URL

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 
 const app = express();
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: process.env.NEXT_PUBLIC_URL,
   credentials: true,
 }));
 
