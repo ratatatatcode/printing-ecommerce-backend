030ff0a2e9da1510afcfac32389bd895374ae532
ratatatcode <pogisijamesmagnaye@gmail.com>
2025-09-01 15:46:51 +0800

    Add cancel order

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/isAuthenticated.js";
-import { getCurrentUser } from "../controllers/user.controllers.js";
+import { getCurrentUser, cancelOrder } from "../controllers/user.controllers.js";
 
 const router = express.Router();
 
 router.get("/current", isAuthenticated, getCurrentUser);
+router.put("/cancel-order", isAuthenticated, cancelOrder);
 
 export default router;
