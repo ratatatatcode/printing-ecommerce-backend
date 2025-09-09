e376c2b288d2757e732693df61d93b95980ea9cc
ratatatcode <pogisijamesmagnaye@gmail.com>
2025-09-09 20:37:24 +0800

    Add create review

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -13,3 +13,39 @@ export const getCurrentUser = async (req, res) => {
         res.status(500).json({ message: "Server error" });
     }
 };
+
+export const addReview = async (req, res) => {
+  const { content, rating } = req.body
+  let reviewUrl = null;
+
+  try { 
+    if (req.file) {
+      const fileName = `${Date.now()}-${req.file.originalname}`;
+
+      const { error } = await supabase.storage
+        .from("reviews")
+        .upload(fileName, req.file.buffer, {
+        contentType: req.file.mimetype,
+        upsert: false,
+      });
+
+      if (error) throw error;
+
+      const { data: publicData } = supabase.storage
+        .from("reviews")
+        .getPublicUrl(fileName);
+
+      reviewUrl = publicData?.publicUrl || null;
+    }
+
+    await pool.query(
+      `INSERT INTO orders
+        (content, rating, reviewUrl) VALUES (?, ?)`, [content, rating, reviewUrl]
+    );
+
+    res.status(201).json({message: "Review created successfully",});
+  } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Server error" });
+  }
+}
