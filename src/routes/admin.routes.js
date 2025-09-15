55184a76fed0113d6492b7b1fb77abd820266f11
ratatatcode <pogisijamesmagnaye@gmail.com>
2025-09-15 23:26:51 +0800

    Update routes

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/isAuthenticated.js";
-import { getAllUsers, getSales, getAllOrders, getReviews, updatePrice, deleteUser } from "../controllers/admin.controller.js";
+import { getAllUsers, getSales, getAllOrders, getReviews, updatePrice, updateStatus, deleteUser } from "../controllers/admin.controller.js";
 
 const router = express.Router();
 
@@ -9,6 +9,7 @@ router.get("/get-sales", isAuthenticated, getSales);
 router.get("/get-orders", isAuthenticated, getAllOrders);
 router.get("/get-reviews", isAuthenticated, getReviews);
 router.put("/update-price", isAuthenticated, updatePrice);
+router.put("/update-status", isAuthenticated, updateStatus);
 router.delete("/delete-user", isAuthenticated, deleteUser);
 
 export default router;
